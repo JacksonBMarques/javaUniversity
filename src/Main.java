6e7131638aa59de47845681bf973dd63a21789e0
Jackson Marques <m.jbrenodev@outlook.com>
2025-08-26 16:44:42 -0300

    Sprint 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Solicita o nome do aluno
+        System.out.print("Digite o nome do aluno: ");
+        String nomeAluno = scanner.nextLine();
+
+        // Define as matérias
+        String[] materias = {"Matemática", "Física", "Química"};
+        int[] notas = new int[materias.length];
+
+        // Solicita as notas para cada matéria
+        for (int i = 0; i < materias.length; i++) {
+            System.out.print("Digite a nota de " + materias[i] + ": ");
+            notas[i] = scanner.nextInt();
+        }
+
+        // Pergunta se o usuário deseja ver as notas
+        System.out.print("\nDeseja ver as notas do aluno? (s/n): ");
+        scanner.nextLine(); // Limpa o buffer
+        String resposta = scanner.nextLine();
+
+        if (resposta.equalsIgnoreCase"s")) {
+            System.out.println("\nNotas da aluna(o) " + nomeAluno + ":");
+            for (int i = 0; i < materias.length; i++) {
+                System.out.println(materias[i] + ": " + notas[i]);
+            }
+        } else {
+            System.out.println("\nObrigado.");
         }
     }
 }
